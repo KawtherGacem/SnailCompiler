347492a3ef15e70caa9292e30bfb3cd3fee71393
Kawther Gacem <kaoutheralbertinagacem@gmail.com>
2022-01-20 15:22:37 +0100

    lexical analyzer done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,22 +2,29 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.spi.FileTypeDetector;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class Controller {
+public class Controller implements Initializable {
+
+    public String ident ="([a-zA-Z])+(_?([a-zA-Z0-9])+)*";
+    public String integer ="[0-9]+" ;
+    public String reel ="[0-9]+.[0-9]+" ;
+    public String comment ="\\w";
+    public String message ="\"([a-zA-Z])+('?([a-zA-Z0-9])+)*\"";
 
     @FXML
     private Button chooseFileBtn;
@@ -29,24 +36,30 @@ public class Controller {
     private TextArea lexicalTextArea;
 
     FileChooser fileChooser = new FileChooser();
+    File file ;
 
-    @FXML
-    public void chooseFileOnClick(ActionEvent event) throws IOException {
-        File file = fileChooser.showOpenDialog(null);
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("snail files", "*.snl"));
         String userHome = System.getProperty("user.home");
         fileChooser.setInitialDirectory(new File(userHome));
-        FileReader fileReader = new FileReader(file);
+    }
+    @FXML
+    public void chooseFileOnClick(ActionEvent event) throws IOException {
+        file = fileChooser.showOpenDialog(null);
         textArea.clear();
         if(file != null){
             Files.lines(file.toPath(), Charset.forName("UTF-8"))
                     .forEach(line -> textArea.appendText(line.concat("\n")));
         }
+        textArea.setEditable(true);
         }
 
     @FXML
-    void saveOnClick(ActionEvent event) {
-
+    void saveOnClick(ActionEvent event) throws IOException {
+        FileWriter fw = new FileWriter(file);
+        fw.write(textArea.getText());
+        fw.close();
     }
 
 
@@ -57,9 +70,77 @@ public class Controller {
         for (int i=0;i<lines.length;i++) {
             String[] words = lines[i].split("\s");
             for (int j = 0; j < words.length; j++) {
-                System.out.println(words[j]);
+                if (words[j].contains(",")){
+                   String[] idents= words[j].split(",");
+                    for (int k = 0; k < idents.length; k++) {
+                        if (Pattern.matches(ident,idents[k])){
+                            hash.put(idents[k],"identifier");}
+                        else {
+                            hash.put(idents[k],"Error, cannot resolve symbol");
+                        }
+                    }
+                }else if (Pattern.matches("Snl_Start",words[j])){
+                    hash.put(words[j],"reserved word for starting program");}
+                else if (Pattern.matches("Snl_Int",words[j])){
+                    hash.put(words[j],"reserved word for declaring an int");}
+                else if (Pattern.matches("%.",words[j])){
+                    hash.put(words[j],"reserved word for end of line");}
+                else if (Pattern.matches("Set",words[j])){
+                    hash.put(words[j],"reserved word for assigning a value");}
+                else if (Pattern.matches("Snl_Real",words[j])){
+                    hash.put(words[j],"reserved word for declaring a reel");}
+                else if (Pattern.matches("if",words[j])){
+                    hash.put(words[j],"reserved word for condition");}
+                else if (Pattern.matches("%",words[j])){
+                    hash.put(words[j],"reserved word for start and end of condition");}
+                else if (Pattern.matches("Else",words[j])){
+                    hash.put(words[j],"reserved word for condition");}
+                else if (Pattern.matches("Start",words[j])){
+                    hash.put(words[j],"reserved word for starting a condition code");}
+                else if (Pattern.matches("Get",words[j])){
+                    hash.put(words[j],"reserved word for getting a value");}
+                else if (Pattern.matches("Finish",words[j])){
+                    hash.put(words[j],"reserved word for ending a condition code");}
+                else if (Pattern.matches("Snl_Put",words[j])){
+                    hash.put(words[j],"reserved word for showing message");}
+                else if (Pattern.matches("\"",words[j])){
+                    hash.put(words[j],"reserved word for starting and ending message");}
+                else if (Pattern.matches("Snl_St",words[j])){
+                    hash.put(words[j],"reserved word for declaring a string");}
+                else if (Pattern.matches("%..",words[j])){
+                    hash.put(words[j],"reserved word for starting a comment");}
+                else if (Pattern.matches("Snl_close",words[j])){
+                    hash.put(words[j],"reserved word for ending program");}
+                else if (Pattern.matches("<",words[j])){
+                    hash.put(words[j],"reserved word for less then");}
+                else if (Pattern.matches(">",words[j])){
+                    hash.put(words[j],"reserved word for greater then");}
+                else if (Pattern.matches("=",words[j])){
+                    hash.put(words[j],"reserved word for equal to");}
+                else if (Pattern.matches("&",words[j])){
+                    hash.put(words[j],"reserved word for AND");}
+                else if (Pattern.matches("\\|",words[j])){
+                    hash.put(words[j],"reserved word for OR");}
+                else if (Pattern.matches(ident,words[j])){
+                    hash.put(words[j],"identifier");}
+                else if (Pattern.matches(integer,words[j])){
+                    hash.put(words[j],"integer");}
+                else if (Pattern.matches(reel,words[j])){
+                    hash.put(words[j],"reel");}
+                else if (Pattern.matches(comment,words[j])){
+                    hash.put(words[j],"comment");}
+                else if (Pattern.matches(message,words[j])){
+                    hash.put(words[j],"message");}
+                else {
+                    hash.put(words[j],"ERROR, cannot resolve symbol");
+                }
             }
         }
+        lexicalTextArea.clear();
+        for (String i : hash.keySet()) {
+            lexicalTextArea.appendText(i + " : " + hash.get(i)+"\n");
+        }
     }
+
 
 }
--- src/sample/userDefined.java
+++ src/sample/userDefined.java
@@ -5,6 +5,25 @@ public class userDefined {
     public String Integer ="[0-9]+" ;
     public String reel ="[0-9]+.[0-9]+" ;
     public String comment ="\\w";
-    public String message ="\"[^\"]+\"";
+    public String message ="\"([a-zA-Z])+('?([a-zA-Z0-9])+)*\"";
 
+    public String getIdent() {
+        return ident;
+    }
+
+    public String getInteger() {
+        return Integer;
+    }
+
+    public String getReel() {
+        return reel;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
